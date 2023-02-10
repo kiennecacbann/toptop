3ab7c04300ff73bfc9ed2a1af849dabe6d67fd57
kienne <nguyenkiencke@gmail.com>
2023-02-10 15:22:17 +0700

    kienne

---
+++ components/SuggestedAccounts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuggestedAccounts = () => {
+  return (
+    <div>Suggested accounts</div>
+  )
+}
+
+export default SuggestedAccounts
---
+++ sanity-backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '2us9bb5y',
+    dataset: 'production'
+  }
+})
